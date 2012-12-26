c0bff602805185532ca5f7d2f7247dc5b4daf4b2
Martin Siniawski <martin@streema.com>
2012-12-26 16:02:05 +0000

    Adding a custom admin class for the EmailConfirmation model.
    Has raw_id_fields option to avoid huge query when fetching existing email addresses rows.

--- emailconfirmation/admin.py
+++ emailconfirmation/admin.py
@@ -2,6 +2,10 @@ from django.contrib import admin
 
 from emailconfirmation.models import EmailAddress, EmailConfirmation
 
+class EmailConfirmationAdmin(admin.ModelAdmin):
+
+        raw_id_fields = ('email_address',)
+
 
 admin.site.register(EmailAddress)
-admin.site.register(EmailConfirmation)
+admin.site.register(EmailConfirmation, EmailConfirmationAdmin)
