8faf660b75c17e9f0c35e468d1afb61866577680
Martin Siniawski <martin@streema.com>
2012-12-26 17:17:42 +0000

    Adding custom admin class to EmailAddress model.
    Uses raw_id_fields attribute to avoid huge query on User model.

--- emailconfirmation/admin.py
+++ emailconfirmation/admin.py
@@ -2,11 +2,17 @@ from django.contrib import admin
 
 from emailconfirmation.models import EmailAddress, EmailConfirmation
 
+
+class EmailAddressAdmin(admin.ModelAdmin):
+
+        raw_id_fields = ('user',)
+
+
 class EmailConfirmationAdmin(admin.ModelAdmin):
 
         search_fields = ('email_address__email', 'email_address__user__username', 'email_address__user__id',)
         raw_id_fields = ('email_address',)
 
 
-admin.site.register(EmailAddress)
+admin.site.register(EmailAddress, EmailAddressAdmin)
 admin.site.register(EmailConfirmation, EmailConfirmationAdmin)
